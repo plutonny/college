71255bfc3d2f9d0f601cb50708ce494a021fefb1
Viktor <plutonny1@gmail.com>
2021-10-03 16:04:30 +0300

    restructurise add PWA (trying) add chace

--- static/frames.js
+++ static/frames.js
@@ -13,30 +13,28 @@ dt = new Date();
     for example: 'general' returned main page
 */
 async function page(current) {
-    if (current === 'general') { header('главная', true); home(); output(true, 'VERSION.html') }
-    else { error(604, 'ERROR: cant find file in page function'); };
+         if (current == 'general')   { header('главная', true); home(); output(true, 'VERSION.html') }
+    else if (current == 'timetable') { error(604) }
+    else if (current == 'other')     { error(604) } 
+    else                             { error(604, 'ERROR: cant find file in page function'); };
     await sleep(50);
     theme(1);
-};
+}
 
-/* 
-    Header function, create header 
-    text - text after logo (can be changed)
+/*
+    PageSet function
+    settings - setting function:
+    "rel" - reload page
+    "tablepos" - redirect to table group
+    "include" - include HTML
+    "week" - returned week ('желтая' or 'зеленая')
 */
-function header(text, enableTheme) {
-    document.write('<div id="header">\
-    <div class="header" style="padding-bottom: 12px; display: flex; flex-direction: row; flex-wrap: nowrap; align-items: center; user-select: none;">\
-        <a href="home.html" class="logo_text_header"><p class="logo_text_header" style="font-size: 24px; margin-bottom: 12px; margin-left: 24px;">Группа КСК-11</p></a>\
-        <p class="logo_dot_header" style="font-size: 26px; margin-bottom: 16px; margin-left: 14px; margin-right: 14px;">•</p>\
-        <div class="mobile_gorisontal_void"></div>\
-        <p style="font-size: 20px; margin-bottom: 8px;">' + text + '</p>\
-        <div class="pc_gorisontal_void"></div>\
-        <div class="mobile_gorisontal_void"></div>');
-    if (enableTheme) { 
-        document.write('<button id="theme_button" class="theme_header_button_pc" onclick="theme(0)">Cменить тему</button>'); 
-        document.write('<button id="theme_button" class="theme_header_button_mobile" onclick="theme(0)"><img style="width: 32px;" id="mobile_theme_button" src=""></button>')
-    };
-    document.write('</div><hr style="border: none; height: 1px; margin-top: 0;"></div>');
+function pageSet(settings) {
+         if (settings == 'tablepos') { parent.open("https://docs.google.com/spreadsheets/d/12NHcO3C-BL2vZ5B64_2nW4RoH-zKdDL4Ixj2MkmWiag/edit") }
+    else if (settings == 'rel')      { console.log('RELOADING PAGE!'); window.location.reload(); }
+    else if (settings == 'week')     { var oneJan = new Date(currentdate.getFullYear(),0,1); var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000)); var result = Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7); if (result % 2 == 1) {return 'желтая'} else {return 'зеленая'} }
+    else if (settings == 'include')  { try { var z, i, elmnt, file, xhttp;z = document.getElementsByTagName("*");for (i = 0; i < z.length; i++) {elmnt = z[i];file = elmnt.getAttribute("include-html");if (file) {xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function() {if (this.readyState == 4) {if (this.status == 200) {elmnt.innerHTML = this.responseText;};if (this.status == 404) {error(602, 'ERROR: innerHTML function cant find file');};elmnt.removeAttribute("include-html");pageSet('include');};};xhttp.open("GET", file, true);xhttp.send();return } } } catch (e) { error(602, e) } }
+    else                             { output(false, 'ERROR: pageSet function not found instruction to "' + settings + '"') }
 }
 
 /* 
@@ -99,29 +97,29 @@ async function theme(type) {
 
     if (type === 0) {
         if (localStorage.getItem('theme').includes('light')) { 
-            try { document.getElementById('mobile_theme_button').src = 'static/sun.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
+            try { document.getElementById('mobile_theme_button').src = 'static/images/sun.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
             document.getElementById('theme_css').innerHTML = dark; 
             localStorage.setItem('theme','dark'); 
         } else if (localStorage.getItem('theme').includes('dark')) { 
-            try { document.getElementById('mobile_theme_button').src = 'static/moon.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
+            try { document.getElementById('mobile_theme_button').src = 'static/images/moon.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
             document.getElementById('theme_css').innerHTML = light; 
             localStorage.setItem('theme','light'); 
         } else {
             console.log('Error: theme are undefined! Setting theme to light...');
-            try { document.getElementById('mobile_theme_button').src = 'static/moon.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
+            try { document.getElementById('mobile_theme_button').src = 'static/images/moon.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
             document.getElementById('theme_css').innerHTML = light; 
             localStorage.setItem('theme','light'); 
         };
     }; if (type === 1) {
         if (localStorage.getItem('theme').includes('light')) { 
-            try { document.getElementById('mobile_theme_button').src = 'static/moon.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
+            try { document.getElementById('mobile_theme_button').src = 'static/images/moon.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
             document.getElementById('theme_css').innerHTML = light;
         } else if (localStorage.getItem('theme').includes('dark')) { 
-            try { document.getElementById('mobile_theme_button').src = 'static/sun.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
+            try { document.getElementById('mobile_theme_button').src = 'static/images/sun.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
             document.getElementById('theme_css').innerHTML = dark; 
         } else {
             console.log('Error: theme are undefined! Setting theme to light...');
-            try { document.getElementById('mobile_theme_button').src = 'static/moon.svg';  } catch { console.log('Warning: theme button is not enabled!'); };
+            try { document.getElementById('mobile_theme_button').src = 'static/images/moon.svg';  } catch { console.log('Warning: theme button is not enabled!'); };
             document.getElementById('theme_css').innerHTML = light; 
             localStorage.setItem('theme','light'); 
         }
@@ -131,7 +129,7 @@ async function theme(type) {
         await sleep(15);
         try {
             localStorage.setItem('theme','light');
-            try { document.getElementById('mobile_theme_button').src = 'static/moon.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
+            try { document.getElementById('mobile_theme_button').src = 'static/images/moon.svg'; } catch { console.log('Warning: theme button is not enabled!'); };
             document.getElementById('theme_css').innerHTML = light;
         } catch (e) {error(601, 'CRITICAL ERROR: theme cannot load, redirect to error page (' + e + ')')}
     }
@@ -148,32 +146,103 @@ function home() {
     else if (dt.getHours() <= 18 && dt.getHours() > 12) { outtext = 'Добрый день!' } 
     else if (dt.getHours() <= 23 && dt.getHours() > 18) { outtext = 'Добрый вечер!'} 
     else                                                { outtext = 'Доброй ночи!' }
-    output(false, `
+    output(false,`
     <div class="greetings"><p style="font-size:28px; margin-bottom:8px;">${outtext}</p></div>
     <div class="dateweek"><p style="font-size:18px; margin-top:4px;">${dt.getDate()} ${month[dt.getMonth()]}, ${pageSet('week')} неделя</p></div>
     <div class="support_table">
-        <a href="https://docs.google.com/spreadsheets/d/12NHcO3C-BL2vZ5B64_2nW4RoH-zKdDL4Ixj2MkmWiag/edit" 
-        style="font-size: 32px;">Таблица посещаемости</a>
-        <button onclick="pageSet('tablepos')">Таблица посещаемости</button>
+        <button class="support_button" onclick="page('timetable')">Расписание</button>
+        <button class="support_button" onclick="pageSet('tablepos')">Таблица посещаемости</button>
+        <button class="support_button" onclick="page('other')">Дополнительно</button>
     </div>
     `)
 }
 
-/*
-    PageSet function
-    settings - setting function:
-    "rel" - reload page
-    "tablepos" - redirect to table group
-    "include" - include HTML
-    "week" - returned week ('желтая' or 'зеленая')
+/* 
+    Header function, create header 
+    text - text after logo (can be changed)
 */
-function pageSet(settings) {
-         if (settings == 'tablepos') { location.replace("https://docs.google.com/spreadsheets/d/12NHcO3C-BL2vZ5B64_2nW4RoH-zKdDL4Ixj2MkmWiag/edit") }
-    else if (settings == 'rel')      { console.log('RELOADING PAGE!'); window.location.reload(); }
-    else if (settings == 'week')     { var oneJan = new Date(currentdate.getFullYear(),0,1); var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000)); var result = Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7); if (result % 2 == 1) {return 'желтая'} else {return 'зеленая'} }
-    else if (settings == 'include')  { try { var z, i, elmnt, file, xhttp;z = document.getElementsByTagName("*");for (i = 0; i < z.length; i++) {elmnt = z[i];file = elmnt.getAttribute("include-html");if (file) {xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function() {if (this.readyState == 4) {if (this.status == 200) {elmnt.innerHTML = this.responseText;};if (this.status == 404) {error(602, 'ERROR: innerHTML function cant find file');};elmnt.removeAttribute("include-html");pageSet('include');};};xhttp.open("GET", file, true);xhttp.send();return } } } catch (e) { error(602, e) } }
-    else                             { output(false, 'ERROR: pageSet function not found instruction to "' + settings + '"') }
+function header(text, enableTheme) {
+    document.write('<div id="header">\
+    <div class="header" style="padding-bottom: 12px; display: flex; flex-direction: row; flex-wrap: nowrap; align-items: center; user-select: none;">\
+        <a href="home.html" class="logo_text_header"><p class="logo_text_header" style="font-size: 24px; margin-bottom: 12px; margin-left: 24px;">Группа КСК-11</p></a>\
+        <p class="logo_dot_header" style="font-size: 26px; margin-bottom: 16px; margin-left: 14px; margin-right: 14px;">•</p>\
+        <div class="mobile_gorisontal_void"></div>\
+        <p style="font-size: 20px; margin-bottom: 8px;">' + text + '</p>\
+        <div class="pc_gorisontal_void"></div>\
+        <div class="mobile_gorisontal_void"></div>');
+    if (enableTheme) { 
+        document.write('<button id="theme_button" class="theme_header_button_pc" onclick="theme(0)">Cменить тему</button>'); 
+        document.write('<button id="theme_button" class="theme_header_button_mobile" onclick="theme(0)"><img style="width: 32px;" id="mobile_theme_button" src=""></button>')
+    };
+    document.write('</div><hr style="border: none; height: 1px; margin-top: 0;"></div>');
 }
 
 /* Sleep function (dont touch this!) */
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+/* Service worker function (dont touch!) */
+function serw() {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+            navigator.serviceWorker.register('/static/service-worker.js').then(function(registration) {
+                // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+          }).catch(function(err) {
+            console.log(err)
+          });
+        });
+      } else {
+        console.log('service worker is not supported');
+      }
+}
+
+/*
+for copy
+
+
+    "icons": [
+          {
+        "src":"/static/core/manifest/logo-pwa-16.png",
+        "sizes": "16x16",
+        "type": "image/png"
+      },
+      {
+        "src":"/static/core/manifest/logo-pwa-32.png",
+        "sizes": "32x32",
+        "type": "image/png"
+      },
+      {
+        "src":"/static/core/manifest/logo-pwa-48.png",
+        "sizes": "48x48",
+        "type": "image/png"
+      },
+      {
+        "src":"/static/core/manifest/logo-pwa-72.png",
+        "sizes": "72x72",
+        "type": "image/png"
+      },
+      {
+        "src":"/static/core/manifest/logo-pwa-96.png",
+        "sizes": "96x96",
+        "type": "image/png"
+      },
+      {
+        "src":"/static/core/manifest/logo-pwa-144.png",
+        "sizes": "144x144",
+        "type": "image/png"
+      },
+      {
+        "src":"/static/core/manifest/logo-pwa-192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src":"/static/core/manifest/logo-pwa-512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ],
+*/
