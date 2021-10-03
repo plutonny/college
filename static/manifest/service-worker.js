e4eb7a3240916708257e89affbb0f6d3aeaa44d1
Viktor <plutonny1@gmail.com>
2021-10-03 16:19:39 +0300

    anyway try to create PWA

--- static/manifest/service-worker.js
+++ static/manifest/service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('install', function(event) {
            caches.open(CACHE_NAME)
                .then(function(cache) {
                    // Получаем данные из манифеста (они кэшируются)
-                   fetch('/static/manifest.json')
+                   fetch('/college/static/manifest/manifest.json')
                        .then(response => {
                            response.json()
                        })
